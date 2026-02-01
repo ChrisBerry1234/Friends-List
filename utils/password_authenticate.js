349f213a939d1de2e2df75b01c0db8a447ac5add
ChrisBerry1234 <christopherjberry846@gmail.com>
2026-02-01 18:50:34 -0500

    Created password_authentication util function to authenticate user request body object consisting of username and password--Verified against incorrect email, correct email, incorrect password and correct password

---
+++ utils/password_authenticate.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const path = require('path')
+const bcrypt = require('bcrypt')
+const FOLDER = '../data'
+const FILE = 'data.json'
+
+const file_path = path.resolve(__dirname, FOLDER, FILE);
+
+const authenticateUser = async (userCredentials) => {
+    try {
+        const raw_data = await fs.promises.readFile(file_path, {encoding: 'utf8', flag: 'r'});
+        //ERROR HANDLE
+        const data = raw_data.length > 0 && JSON.parse(raw_data)
+        const users = data.users;
+        
+        //DECONSTRUCT USERNAME OBJECT
+        const {email, password} = userCredentials;
+
+        const user = users.find(person => person.email === email);
+        if(!user){
+            return {"Success": false, "message": "User not found"};
+        }
+        //ELSE
+        try{
+            if( await bcrypt.compare(password,user.password)) return {"Success": true, "message": "User authenticated"};
+            else return {"Success": false, "message": "Password incorrect, User not Allowed"};
+        }
+        catch(err){
+            return err.message;
+        }
+    }
+    catch(err){
+        console.log(err.message)
+    }
+}
+
+const user = {
+    "email": "jonlovato@theworld.com",
+    "password": "myPassword"
+}
+
+const result = authenticateUser(user).then(response => console.log(response));
